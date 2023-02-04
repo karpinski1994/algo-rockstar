3b99e435daa6ab1420307cf9587eed7defd48e9d
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-02-04 17:03:31 -0500

    Update readme for new conventions, apply them immidiately by extracting nav settings file

--- components/TabsNavbar/TabsNavbar.tsx
+++ components/TabsNavbar/TabsNavbar.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React from 'react'
 import { Nav } from 'react-bootstrap'
+import navItems from './settings';
 type Props = {
     queryElement: any
 }
@@ -10,11 +11,6 @@ const addActiveClass = (currentUrl: string, linkUrl: string) => currentUrl === l
 function TabsNavbar({ queryElement }: Props) {
     const router = useRouter();
     const { asPath } = router;
-    const navItems = [
-        { name: "Description", href: "" },
-        { name: "Vizualization", href: "vizualization" },
-        { name: "Code Examples", href: "code-examples" },
-      ];
     return (
     <Nav fill variant="tabs">
         {
---
+++ components/TabsNavbar/settings.ts
@@ -0,0 +1,5 @@
+export default [
+    { name: "Description", href: "" },
+    { name: "Vizualization", href: "vizualization" },
+    { name: "Code Examples", href: "code-examples" },
+  ];
