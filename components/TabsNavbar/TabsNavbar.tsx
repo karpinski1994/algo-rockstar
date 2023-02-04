8f2760efad930b85c0bdf5d60b43ef2ceb73deec
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-02-03 21:00:47 -0500

    Remove logs, handle highlighting the tab according to currently visited url

--- components/TabsNavbar/TabsNavbar.tsx
+++ components/TabsNavbar/TabsNavbar.tsx
@@ -5,22 +5,28 @@ type Props = {
     queryElement: any
 }
 
+const addActiveClass = (currentUrl: string, linkUrl: string) => currentUrl === linkUrl ? 'active' : '';
 
 function TabsNavbar({ queryElement }: Props) {
     const router = useRouter();
+    const { asPath } = router;
 
     return <Nav fill variant="tabs"
     >
-         <Nav.Item>
+        <Nav.Item>
             <Nav.Link
+                // TODO: Maybe move it to utils, and extract configuration of nav to some config array
+                className={addActiveClass(asPath, `/data-structures/${queryElement}`)}
                 href={`/data-structures/${queryElement}/`}>Description</Nav.Link>
         </Nav.Item>
         <Nav.Item>
             <Nav.Link
+                className={addActiveClass(asPath, `/data-structures/${queryElement}/vizualization`)}
                 href={`/data-structures/${queryElement}/vizualization`}>Vizualization</Nav.Link>
         </Nav.Item>
         <Nav.Item>
             <Nav.Link
+                className={addActiveClass(asPath, `/data-structures/${queryElement}/code-examples`)}
                 href={`/data-structures/${queryElement}/code-examples`}>Code Examples</Nav.Link>
         </Nav.Item>
     </Nav>
