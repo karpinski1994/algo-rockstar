ca1a35229f2455a8f9318d97129f84a72e2f1090
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-02-08 16:25:33 -0500

    Clean visualization functionality until some point

--- components/Step/index.tsx
+++ components/Step/index.tsx
@@ -2,13 +2,60 @@ import ArrayVisualization from "@/data-structures/Array/visualization";
 import LinkedlistVisualization from "@/data-structures/LinkedList/visualization";
 
 
+const stepsSettingsForEachJustTest = [
+    {
+      stepId: 0,
+      text: '"Lists" are one type of data structure, and can storemultiple values',
+      nodes: [
+        {
+          name: 'blue',
+          color: [40, 148, 255, 1],
+          arrow: false,
+        },
+        {
+          name: 'blue',
+          color: [50, 18, 255, 1],
+          arrow: false,
+        },
+        {
+          name: 'blue',
+          color: [150, 180, 55, 1],
+          arrow: false,
+        }
+      ]
+    },
+    {
+      stepId: 1,
+      text: 'Second description',
+      nodes: [
+        {
+          name: 'blue',
+          color: [60, 48, 5, 1],
+          arrow: false,
+        },
+        {
+          name: 'blue',
+          color: [200, 73, 255, 1],
+          arrow: false,
+        },
+        {
+          name: 'blue',
+          color: [255, 12, 155, 1],
+          arrow: false,
+        }
+      ]
+    }
+  
+  ]
+  
+
 {/*TODO: add more conditional statements here*/}
-export default function Step({route,stepIndex}){
-    switch(route.route){
+export default function stepsFactory({route}){
+    switch(route){
         case "array":
-            return <ArrayVisualization stepIndex={stepIndex}/>;
+            return stepsSettingsForEachJustTest;
         case "linked list":
-            return <LinkedlistVisualization stepIndex={stepIndex}/>;
+            return stepsSettingsForEachJustTest;
         default:
             return null;
     }
--- data-structures/LinkedList/visualization.tsx
+++ data-structures/LinkedList/visualization.tsx
@@ -1,168 +1,73 @@
 import NodeBox from '@/components/NodeBox/NodeBox'
 import React from 'react'
 
-function LinkedlistVisualization({stepIndex}) {
-  return (
-          steps[stepIndex]
-  )
+
+const stepsSettings = [
+  {
+    stepId: 0,
+    text: '"Lists" are one type of data structure, and can storemultiple values',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: false,
+      },
+      {
+        name: 'blue',
+        color: [50, 18, 255, 1],
+        arrow: false,
+      },
+      {
+        name: 'blue',
+        color: [150, 180, 55, 1],
+        arrow: false,
+      }
+    ]
+  },
+  {
+    stepId: 1,
+    text: 'Second description',
+    nodes: [
+      {
+        name: 'blue',
+        color: [60, 48, 5, 1],
+        arrow: false,
+      },
+      {
+        name: 'blue',
+        color: [200, 73, 255, 1],
+        arrow: false,
+      },
+      {
+        name: 'blue',
+        color: [255, 12, 155, 1],
+        arrow: false,
+      }
+    ]
+  }
+
+]
+
+
+const Step = ({ nodes, text }) => (
+  <div>
+    <div className="d-flex flex-column flex-md-row justify-content-center">
+      {nodes.map(({ name, color, arrow }) =>
+        <NodeBox name={name} color={color} arrow={arrow} />
+      )}
+    </div>
+    <p>{text}</p>
+  </div >
+)
+// TODO: This is wrong visualization must be common and generic
+// we pass some kind of steps that are dynamically chosen by route
+// look at /Users/karpinski94/projects/algo-rockstar/components/CodeExample
+// or /Users/karpinski94/projects/algo-rockstar/components/Visualization
+// and just copy the architecture
+function LinkedlistVisualization() {
+  const stepId = 0;
+  return stepsSettings && stepsSettings[stepId] && <Step {...stepsSettings[stepId]} />
+   
 }
 
 export default LinkedlistVisualization
-
-const steps = [
-  //step 1
-  ( <div>
-      <div className="d-flex flex-column flex-md-row justify-content-center">
-        <NodeBox name="blue" color={[40, 148, 255, 1]}/>
-        <NodeBox name="yellow" color={[255, 171, 1, 1]} />
-        <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-      </div>
-      <p>"Lists" are one type of data structure, and can storemultiple values</p>
-    </div>
-  ),
-  //step 2
-  ( <div>
-      <div className="d-flex justify-content-center">
-        <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-        <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-        <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false} pointed={true}/>
-      </div>
-    <p>They are unique in how they pair data with "pointers", the pointers indicating the next piece of data's memory location</p>
-  </div>
-),
-  //step 3
-  ( <div>
-    <div className="d-flex justify-content-center flex-column align-items-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]}  arrow={false} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]}  arrow={false} pointed={true}/>
-      <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false} pointed={true}/>
-    </div>
-  <p>In lists, data is stored in various disjointed locations in memory.</p>
-</div>
-),
-  //step 4
-  ( <div>
-    <div className="d-flex justify-content-center flex-column align-items-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]}  arrow={false} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]}  arrow={false} pointed={true}/>
-      <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false} pointed={true}/>
-    </div>
-  <p>In lists, data is stored in various disjointed locations in memory.</p>
-</div>
-),
-  //step 5
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]}/>
-      <NodeBox name="yellow" color={[170, 170, 170, 1]}/>
-      <NodeBox name="red" color={[170, 170, 170, 1]} arrow={false}/>
-    </div>
-  <p>Because data is stored in different locations,each piece of data can only be accessed through the ointer that precedes it.</p>
-</div>
-),
-  //step 6
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-      <NodeBox name="yellow" color={[170, 170, 170, 1]}/>
-      <NodeBox name="red" color={[170, 170, 170, 1]} arrow={false}/>
-    </div>
-  <p>Because data is stored in different locations,each piece of data can only be accessed through the ointer that precedes it.</p>
-</div>
-),
-  //step 7
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]}/>
-      <NodeBox name="red" color={[170, 170, 170, 1]} arrow={false}/>
-    </div>
-  <p>Because data is stored in different locations,each piece of data can only be accessed through the ointer that precedes it.</p>
-</div>
-),
-  //step 8
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-      <NodeBox name="red" color={[170, 170, 170, 1]} arrow={false}/>
-    </div>
-  <p>Because data is stored in different locations,each piece of data can only be accessed through the ointer that precedes it.</p>
-</div>
-),
-  //step 9
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-      <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-    </div>
-  <p>Because data is stored in different locations,each piece of data can only be accessed through the ointer that precedes it.</p>
-</div>
-),
-  //step 10
-( <div>
-  <div className="d-flex justify-content-center">
-    <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-    <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-    <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-  </div>
-  <div className="position-relative start-50">
-    <NodeBox name="green" color={[12, 180, 0, 1]} arrow={false} pointed={true}/>
-  </div>
-<p>Addition of data is performed simply by replacing the pointers on either side of the addition.</p>
-</div>
-),
-  //step 11
-  ( <div>
-    <div className="d-flex justify-content-center">
-      <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-      <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-      <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-    </div>
-    <div className="position-relative start-50">
-      <NodeBox name="green" color={[12, 180, 0, 1]} arrow={false} pointed={true}/>
-    </div>
-  <p>Addition of data is performed simply by replacing the pointers on either side of the addition.</p>
-  </div>
-  ),
-    //step 12
-( <div>
-  <div className="d-flex justify-content-center">
-    <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-    <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-    <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-  </div>
-  <div className="position-relative start-50">
-    <NodeBox name="green" color={[12, 180, 0, 1]} arrow={false} pointed={true}/>
-  </div>
-<p>Addition of data is performed simply by replacing the pointers on either side of the addition.</p>
-</div>
-),
-    //step 12
-( <div>
-  <div className="d-flex justify-content-center">
-    <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-    <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-    <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-  </div>
-  <div className="position-relative start-50">
-    <NodeBox name="green" color={[12, 180, 0, 1]} arrow={false} pointed={true}/>
-  </div>
-<p>Addition of data is performed simply by replacing the pointers on either side of the addition.</p>
-</div>
-),
-    //step 13
-    ( <div>
-      <div className="d-flex justify-content-center">
-        <NodeBox name="blue" color={[40, 148, 255, 1]} pointed={true}/>
-        <NodeBox name="yellow" color={[255, 171, 1, 1]} pointed={true}/>
-        <NodeBox name="red" color={[226, 35, 35, 1]} arrow={false}/>
-      </div>
-      <div className="position-relative start-50">
-        <NodeBox name="green" color={[12, 180, 0, 1]} arrow={false} pointed={true}/>
-      </div>
-    <p>This concludes the explanation of lists.</p>
-    </div>
-    ),
-]
