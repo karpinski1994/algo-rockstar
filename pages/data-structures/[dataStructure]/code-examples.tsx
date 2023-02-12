1150910e04b907294f878afebbcd75331aa39e5d
Daniel <henio09@wp.pl>
2023-02-12 12:27:40 +0100

    added prerendering to code-example

--- pages/data-structures/[dataStructure]/code-examples.tsx
+++ pages/data-structures/[dataStructure]/code-examples.tsx
@@ -1,4 +1,6 @@
 import CodeExample from "@/components/CodeExample";
+import codeExampleFactory from "@/components/CodeExample/codeExamplesFactory";
+import { buildUrl, stackNavSettings } from "@/components/StackNavbar/settings";
 import { DataStructurePageLayout } from ".";
 
 const DataStructuresCodeExamplesPage = () => <CodeExample />;
@@ -6,3 +8,29 @@ const DataStructuresCodeExamplesPage = () => <CodeExample />;
 DataStructuresCodeExamplesPage.getLayout = DataStructurePageLayout;
 
 export default DataStructuresCodeExamplesPage;
+
+export const getStaticPaths = () => {
+    const paths = stackNavSettings.filter(({url}) => url.length > 1).map(({ url }) => {
+      return buildUrl(url)
+    })
+    return {
+      paths,
+      fallback: false
+    }
+  }
+
+export async function getStaticProps(context: any) {
+    console.log(context);
+    const {
+      params: {
+        dataStructure
+      }
+    } = context;
+    const codeExample = codeExampleFactory(context.params.dataStructure);
+    return {
+      props: {
+        dataStructure,
+        codeExample
+      },
+    };
+  }
