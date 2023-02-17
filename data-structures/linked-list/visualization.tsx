58fc09dfecfcc7d7b6bda82ca2d529239b41bd88
Daniel <henio09@wp.pl>
2023-02-17 21:10:08 +0100

    more elements in linked-list

--- data-structures/linked-list/visualization.tsx
+++ data-structures/linked-list/visualization.tsx
@@ -68,6 +68,166 @@ const linkedListVisualizationSteps = [
         pointed: true
       }
     ]
+  },
+  {
+    stepId: 3,
+    text: `In lists, data is stored in various disjointed locations in memory.`,
+    orientation: 'column',
+    label: 'Memory',
+    nodes: [
+      {
+        name: 'yellow',
+        color: [255, 200, 10, 1],
+        arrow: false,
+        pointed: true
+      },
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: false,
+        pointed: true
+      },
+      {
+        name: 'red',
+        color: [240, 10, 10, 1],
+        arrow: false,
+        pointed: true
+      }
+    ]
+  },
+  {
+    stepId: 4,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+      },
+      {
+        name: 'yellow',
+        color: [180, 180, 180, 1],
+        arrow: true,
+      },
+      {
+        name: 'red',
+        color: [180, 180, 180, 1],
+      }
+    ]
+  },
+  {
+    stepId: 5,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'yellow',
+        color: [180, 180, 180, 1],
+        arrow: true,
+      },
+      {
+        name: 'red',
+        color: [180, 180, 180, 1],
+      }
+    ]
+  },
+  {
+    stepId: 6,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'yellow',
+        color: [255, 200, 10, 1],
+        arrow: true,
+      },
+      {
+        name: 'red',
+        color: [180, 180, 180, 1],
+      }
+    ]
+  },
+  {
+    stepId: 7,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'yellow',
+        color: [255, 200, 10, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'red',
+        color: [180, 180, 180, 1],
+      }
+    ]
+  },
+  {
+    stepId: 8,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'yellow',
+        color: [255, 200, 10, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'red',
+        color: [240, 10, 10, 1],
+      }
+    ]
+  },
+  {
+    stepId: 9,
+    text: `Because data is stored in different locations, each piece of data can only be accessed through the pointer that precedes it.`,
+    label: 'Sequential Access',
+    nodes: [
+      {
+        name: 'blue',
+        color: [40, 148, 255, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'yellow',
+        color: [255, 200, 10, 1],
+        arrow: true,
+        pointed: true
+      },
+      {
+        name: 'red',
+        color: [240, 10, 10, 1],
+      }
+    ]
   }
 
 ]
