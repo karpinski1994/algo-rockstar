b1970ba3a662380011bd8ccaf21ba76732f5a2fb
Daniel <henio09@wp.pl>
2023-03-07 12:19:14 +0100

    add interface to Visualization

--- components/Visualization/index.tsx
+++ components/Visualization/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import StepsSlider from '../StepsSlider';
+import React from "react";
+import StepsSlider from "../StepsSlider";
 
-export const Visualization = ({visualization}) => {
-    return (
-        <>
-            
-            {visualization && <StepsSlider visualization={visualization} />}
-        </>
-    )
+interface VisualizationRows {
+  name: string;
+  color: number[];
+  pointer: boolean;
 }
+
+interface VisualizationSteps {
+  text: string;
+  rows: Array<VisualizationRows>;
+  stepId: number;
+  orientation?: string;
+}
+
+export const Visualization = ({
+  visualization,
+}: {
+  visualization: Array<VisualizationSteps>;
+}) => {
+  return <>{visualization && <StepsSlider visualization={visualization} />}</>;
+};
