8d617a21e7acb7a2f96756f6dd8844d69d58ed7c
Daniel <henio09@wp.pl>
2023-03-09 19:10:14 +0100

    making a queue feature

--- components/Visualization/visualizationFactory.tsx
+++ components/Visualization/visualizationFactory.tsx
@@ -1,6 +1,7 @@
 import arrayVisualizationSteps from "@/data-structures/array/visualization";
 import linkedListVisualizationSteps from "@/data-structures/linked-list/visualization";
 import stackVisualizationSteps from "@/data-structures/stack/visualization";
+import queueVisualizationSteps from"@/data-structures/queue/visualization";
 
 const visualizationFactory = (codeStructure: string | string[] | undefined) => {
   switch (codeStructure) {
@@ -10,6 +11,8 @@ const visualizationFactory = (codeStructure: string | string[] | undefined) => {
       return arrayVisualizationSteps;
     case "stack":
       return stackVisualizationSteps;
+    case "queue":
+      return queueVisualizationSteps;
     default:
       return "Content for that section will be provided soon.";
   }
