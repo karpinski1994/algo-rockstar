45feba73f652c321bf49919c33e98f9658c18fe1
Daniel <henio09@wp.pl>
2023-03-18 17:42:09 +0100

    added queue description

--- components/Markdown/markdownFactory.ts
+++ components/Markdown/markdownFactory.ts
@@ -1,6 +1,7 @@
-import linkedListMarkdown from '../../data-structures/linked-list/description';
-import arrayMarkDown from '../../data-structures/array/description';
-import bigODecription from '../../data-structures/big-o/description';
+import linkedListMarkdown from "../../data-structures/linked-list/description";
+import arrayMarkDown from "../../data-structures/array/description";
+import bigODecription from "../../data-structures/big-o/description";
+import queueDescriptions from "../../data-structures/queue/description";
 // TODO: directories structure may be a little bit confusing we have 'data-structures' twice
 // we have to think about some declarations folder or settings or whatever
 // (TO BE CHECKED WHAT ARE GOOD PRACTICES)
@@ -12,8 +13,10 @@ const markdownFactory = (route: string | string[] | undefined) => {
       return arrayMarkDown;
     case "big-o":
       return bigODecription;
+    case "queue":
+      return queueDescriptions;
     default:
-      return 'Content for that section will be provided soon.';
+      return "Content for that section will be provided soon.";
   }
 };
 
