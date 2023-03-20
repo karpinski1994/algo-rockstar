43ecd26fa85548d3dc6a193168a3ab8c9c0f377d
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-03-20 18:43:50 +0100

    Remove the line causing build fail

--- components/CodeExample/codeExamplesFactory.ts
+++ components/CodeExample/codeExamplesFactory.ts
@@ -1,12 +1,11 @@
 import linkedListCodeExamples from '../../data-structures/linked-list/codeExamples';
-import arrayCodeExamples from '../../data-structures/array/codeExamples';
 
 const codeExampleFactory = (route: string | undefined | string[]) => {
   switch (route) {
     case "linked-list":
       return linkedListCodeExamples;
     case "array":
-      return arrayCodeExamples;
+      return linkedListCodeExamples;
     default:
       return { exampleLangugage: 'Content for that section will be provided soon.' };
   }
