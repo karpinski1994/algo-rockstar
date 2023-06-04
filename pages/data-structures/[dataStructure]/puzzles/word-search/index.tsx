7b6f5428d44eacfe09579ee2d231274ae6ba22db
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-06-04 21:22:40 +0200

    Add piece of a description of the puzzle problem

--- pages/data-structures/[dataStructure]/puzzles/word-search/index.tsx
+++ pages/data-structures/[dataStructure]/puzzles/word-search/index.tsx
@@ -20,6 +20,15 @@ export default function WordSearchPage() {
 
   return (
     <div className="App">
+      <h1>Leet code - Word Search</h1>
+      <h4>Description</h4>
+      <p>
+        Given an m x n grid of characters board and a string word, return true
+        if word exists in the grid. The word can be constructed from letters of
+        sequentially adjacent cells, where adjacent cells are horizontally or
+        vertically neighboring. The same letter cell may not be used more than
+        once.
+      </p>
       <input value={text} onChange={(e) => setText(e.target.value)} />
       {fields.map((r: Array<any>, i: number) => (
         <div
@@ -29,7 +38,7 @@ export default function WordSearchPage() {
           }}
           key={new Date().getTime() + i}
         >
-          {r.map((l: {letter: string, isUsed: boolean}, i: number) => (
+          {r.map((l: { letter: string; isUsed: boolean }, i: number) => (
             <>
               <NodeBox
                 width="40%"
