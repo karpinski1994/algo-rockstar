58a88d242bdeb136653cbf981734c60708c9d39e
Patryk Karpinski <patryk.karpinski@outlook.com>
2023-07-13 09:17:23 +0200

    fix bug

--- data-structures/graph/codeExamples.ts
+++ data-structures/graph/codeExamples.ts
@@ -2,35 +2,36 @@
 export default {
     javascript: `
     class UndirectedGraph {
-        constructor() {
-          this.vertices = new Map();
-        }
-      
-        addVertex(vertex) {
-          this.vertices.set(vertex, []);
-        }
-      
-        addEdge(origin, destination) {
-          this.vertices.get(origin).push(destination);
-          this.vertices.get(destination).push(origin);
-        }
-      
-        [inspectionSymbol]() {
-          return this.vertices;
-        }
+      constructor() {
+        this.vertices = new Map();
       }
-      
-      const graph = new UndirectedGraph();
-      graph.addVertex(1);
-      graph.addVertex(2);
-      graph.addVertex(3);
-      graph.addVertex(4);
-      graph.addEdge(1, 2);
-      graph.addEdge(3, 4);
-      graph.addEdge(3, 1);
-      graph.addEdge(3, 2);
-      graph.addEdge(4, 2);
-      console.log(graph);
+    
+      addVertex(vertex) {
+        this.vertices.set(vertex, []);
+      }
+    
+      addEdge(origin, destination) {
+        this.vertices.get(origin).push(destination);
+        this.vertices.get(destination).push(origin);
+      }
+    
+      [Symbol.for("nodejs.util.inspect.custom")]() {
+        return this.vertices;
+      }
+    }
+    
+    const graph = new UndirectedGraph();
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addEdge(1, 2);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 1);
+    graph.addEdge(3, 2);
+    graph.addEdge(4, 2);
+    console.log(graph);
+    
     /*
         Map(4) {
             1 => [ 2, 3 ],
